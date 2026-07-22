5575b3bf6f9af523c37a7b9246968be5f6ecbb62
AnastasiaRoberz <a.roberz@gmx.de>
2026-07-22 21:44:40 +0200

    add data

---
+++ scripts/data.js
@@ -0,0 +1,119 @@
+let menu = [
+    {
+        "category": "Burger & Sandwiches",
+        "iconFileName": "icon-burger.svg",
+        "additionalInfo": "",
+        "dishes": [
+            {
+                "name": "Veggie mushroom black burger",
+                "description": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+                "fileName": "veggie-mushroom.jpg",
+                "price": 16.90,
+                "added": false,
+                "amount": 0
+            },
+            {
+                "name": "All meat burger",
+                "description": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
+                "fileName": "all-meat-burger.jpg",
+                "price": 15.90,
+                "added": false,
+                "amount": 0
+            },
+            {
+                "name": "Beef red burger",
+                "description": "Beef, Cheese, Tomatoes, Lettuce, Onion",
+                "fileName": "beef-red-burger.jpg",
+                "price": 14.90,
+                "added": false,
+                "amount": 0
+            },
+            {
+                "name": "Big chicken burger",
+                "description": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper, ",
+                "fileName": "big-chicken-burger.jpg",
+                "price": 15.90,
+                "added": false,
+                "amount": 0
+            },
+        ]
+    },
+    {
+        "category": "Pizza",
+        "iconFileName": "icon-pizza.svg",
+        "additionalInfo": "30cm",
+        "dishes": [
+            {
+                "name": "Pizza Margherita",
+                "description": "Tomato Sauce, Mozzarella",
+                "fileName": "margherita.jpg",
+                "price": 11.90,
+                "added": false,
+                "amount": 0
+            },
+            {
+                "name": "Pizza Chorizo",
+                "description": "Tomato slices, Mozzarella, Chorizo",
+                "fileName": "chorizo.jpg",
+                "price": 13.90,
+                "added": false,
+                "amount": 0
+            },
+            {
+                "name": "Funghi",
+                "description": "Red onion, Olives, Button Mushrooms, Mozzarella",
+                "fileName": "funghi.jpg",
+                "price": 12.90,
+                "added": false,
+                "amount": 0
+            },
+            {
+                "name": "Quattro Formaggi with Chicken ",
+                "description": "Chicken, Mozzarella, Gorgonzola, Fontina,  Parmigiano Reggiano",
+                "fileName": "quattro-fromaggi.jpg",
+                "price": 15.90,
+                "added": false,
+                "amount": 0
+            }
+        ]
+    },
+    {
+        "category": "Salad",
+        "iconFileName": "icon-salad.svg",
+        "additionalInfo": "",
+        "dishes": [
+            {
+                "name": "Warm beef arugula salad",
+                "description": "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
+                "fileName": "arugula-salad.jpg",
+                "price": 16.90,
+                "added": false,
+                "amount": 0
+            },
+            {
+                "name": "Mini green Salad",
+                "description": "Green salad, Cucumber, Carrots, Parsley, Radishes ",
+                "fileName": "mini-green-salad.jpg",
+                "price": 7.90,
+                "added": false,
+                "amount": 0
+            },
+            {
+                "name": "Green Salad with sea food",
+                "description": "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
+                "fileName": "green-salad.jpg",
+                "price": 16.90,
+                "added": false,
+                "amount": 0
+            },
+            {
+                "name": "Vegan green salad with tofu",
+                "description": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+                "fileName": "vegan-green-salad.jpg",
+                "price": 14.90,
+                "added": false,
+                "amount": 0
+            }
+        ]
+    }
+]
